2b47edd2f5e1f68c00adada3c6c5dba6d07cb9e7
JoshuaBentley <joshuabentley911@gmail.com>
2022-11-21 11:18:11 -0700

    have all my prompts and my confirms added in to the js code

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var myNewPassword = ""
+var choiceArr = []
+var specialCharArr = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", ":", ";", "~" ];
+var uppercasechArarr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercasechArarr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberSetArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 function generatePassword() {
- var answer = prompt("Enter the number of characters you want in your password");
- if (answer < "8"){
-  prompt ("password must be atleast 8 characters!")
+     myNewPassword = prompt("Enter the number of characters you want in your myNewPassword");
+ if (myNewPassword < 8 || myNewPassword > 128){
+     alert ("myNewPassword must be between 8-128 characters, please try again!");
  }
 
-  
-  
+else if (myNewPassword > 8 && myNewPassword < 129) {
+     uppercasechararr = confirm ("Would you like uppercase letters in ypur myNewPassword?");
+}
 
-  return "This is your new password";
+if (myNewPassword) {
+    myNewPassword = confirm("Would you like to use lowercase letters in your myNewPassword?")
+}
+
+if (myNewPassword) {
+    myNewPassword = confirm("Would you like Special characters in your myNewPassword?")
+}
+
+if (myNewPassword) {
+   myNewPassword = confirm("Would you like numbers in your myNewPassword?")
+}
+
+if (myNewPassword) {
+  return Math.floor(Math.random() * myNewPassword)
+}
+
 }
 
 // Write password to the #password input
