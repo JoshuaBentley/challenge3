9bd25a9ae722eb4f23aa09f5f1b7ab13426af8ee
JoshuaBentley <joshuabentley911@gmail.com>
2022-11-22 13:13:17 -0700

    trying to get the password to generate

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var myNewPassword = ""
-var choiceArr = []
 var specialCharArr = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", ":", ";", "~" ];
 var uppercasechArarr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercasechArarr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -15,3 +12,26 @@ if (myNewPassword < 8 || myNewPassword > 128){
  }
  return myNewPassword;
 }
+
+function generatePassword()
+var myNewPassword = ""
+var choiceArr = []
+
+
+
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
