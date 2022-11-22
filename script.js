331f06d38e9e8fb7da481afd912fbd279f3382d4
JoshuaBentley <joshuabentley911@gmail.com>
2022-11-22 12:48:59 -0700

    concat my var

--- script.js
+++ script.js
@@ -8,41 +8,39 @@ var lowercasechArarr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 var numberSetArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 function generatePassword() {
-     myNewPassword = prompt("Enter the number of characters you want in your myNewPassword");
- if (myNewPassword < 8 || myNewPassword > 128){
-     alert ("myNewPassword must be between 8-128 characters, please try again!");
- }
+myNewPassword = prompt("Enter the number of characters you want in your myNewPassword");
+if (myNewPassword < 8 || myNewPassword > 128){
+    alert ("myNewPassword must be between 8-128 characters, please try again!");
+}
 
 else if (myNewPassword > 8 && myNewPassword < 129) {
-     uppercasechararr = confirm ("Would you like uppercase letters in ypur myNewPassword?");
+   myNewPassword = confirm ("Would you like uppercase letters in ypur myNewPassword?");
+    
 }
 
 if (myNewPassword) {
-    myNewPassword = confirm("Would you like to use lowercase letters in your myNewPassword?")
+   myNewPassword = confirm("Would you like to use lowercase letters in your myNewPassword?")
 }
 
 if (myNewPassword) {
-    myNewPassword = confirm("Would you like Special characters in your myNewPassword?")
+   myNewPassword = confirm("Would you like Special characters in your myNewPassword?")
 }
 
 if (myNewPassword) {
-   myNewPassword = confirm("Would you like numbers in your myNewPassword?")
+  myNewPassword = confirm("Would you like numbers in your myNewPassword?")
 }
 
 if (myNewPassword) {
-  return Math.floor(Math.random() * myNewPassword)
 }
 
+return myNewPassword = choiceArr.concat(uppercasechArarr, lowercasechArarr, specialCharArr, numberSetArr)
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
