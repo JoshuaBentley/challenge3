eb888e79f7a5cfe7ece1d059e714886f4b72e5c7
JoshuaBentley <joshuabentley911@gmail.com>
2022-11-22 12:58:39 -0700

    test

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
-var specialCharArr = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", ":", ";", "~" ];
-var uppercasechArarr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercasechArarr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numberSetArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 function getPasswordCriteria() {
-    var userChoseNumbers = confirm("Do you want numbers in your password?");
-    var options = {
-      userChoseNumbers: userChoseNumbers
-    };
-    return options;
-  }
-  
-  function createRandom(length) {
-    var randomNum = Math.floor(Math.random() * length);
-    return randomNum;
-  }
-  
-  function generatePassword() {
-      var userChose = getPasswordCriteria();
-      var availableChars = [];
-      var passwordArr = [];
-      if(userChose.userChoseNumbers) {
-        availableChars = availableChars.concat(numbers);
-      }
-  
-      for(var i = 0; i < 4; i++) {
-        passwordArr.push(availableChars[createRandom(availableChars.length)])
-      }
-  
-  
-    return passwordArr.join("")
-  }
-  
-  // Assignment Code
-  var generateBtn = document.querySelector("#generate");
-  
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+  var userChoseNumbers = confirm("Do you want numbers in your password?");
+  var options = {
+    userChoseNumbers: userChoseNumbers
+  };
+  return options;
+}
+
+function createRandom(length) {
+  var randomNum = Math.floor(Math.random() * length);
+  return randomNum;
+}
+
+function generatePassword() {
+    var userChose = getPasswordCriteria();
+    var availableChars = [];
+    var passwordArr = [];
+    if(userChose.userChoseNumbers) {
+      availableChars = availableChars.concat(numbers);
+    }
+
+    for(var i = 0; i < 4; i++) {
+      passwordArr.push(availableChars[createRandom(availableChars.length)])
+    }
+
+
+  return passwordArr.join("")
+}
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
