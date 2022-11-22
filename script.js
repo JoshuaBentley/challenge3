2a1f3d7ce34e017f04a77f2292808e9ee5b4db5b
JoshuaBentley <joshuabentley911@gmail.com>
2022-11-22 15:19:13 -0700

    starting from scratch togenerate one thing at a time

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var myNewPassword = "";
-var choiceArr = [];
 var specialCharArr = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", ":", ";", "~" ];
 var uppercasechArarr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercasechArarr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberSetArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 function createdPassword() {
-  myNewPassword = prompt("Enter the number of characters you want in your myNewPassword");
-if (myNewPassword < 8 || myNewPassword > 128){
-    alert ("myNewPassword must be between 8-128 characters, please try again!");
-} else if (myNewPassword > 8 && myNewPassword < 129, confirm("Would you like uppercase letters in ypur myNewPassword?"));
-      myNewPassword = choiceArr;
-      console.log(myNewPassword);
-
+   var confirmedUppercaseLetters  = confirm("would you like uppercase letters on your password?");
+   var passwordOptions = {
+      confirmedUppercaseLetters: confirmedUppercaseLetters
+   };
+   return passwordOptions;     
 }
+
+function createRandom(length) {
+   var randomPassword = Math.floor(Math.random() * length);
+   return randomPassword;
+}
+
 function generatePassword() {
+   var userCreatedPassword = getPasswordCriteria();
+   var chosenCharacterArr = [];
+   var createdPasswordArr = [];
+   if(userCreatedPassword.confirmedUppercaseLetters) {
+     chosenCharacterArr = chosenCharacterArr.concat;(uppercasechArarr);
+   }
+
+   for(var i = 0; i < 4; i++) {
+     createdPasswordArr.push(chosenCharacterArr[createRandom(availableChars.length)])
+   }
+
+
+ return createdPasswordArr.join("")
 }
 
 // Write password to the #password input
